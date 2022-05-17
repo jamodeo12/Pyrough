2d64a8d791ceeb48ea2374876c5d4615116f663f
hiteney <hugo.iteney@im2np.fr>
2022-05-17 11:10:15 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="Pyrough",
+version="1.0",
+description="",
+author="Hugo Iteney",
+author_email="hugo.iteney@im2np.fr",
+packages=[],
+install_requires=[],
+extras_require={},
+license="Apache 2.0")
