0375d6a9ddf59103aecd6b18b1db0698c96110ea
hiteney <hugo.iteney@im2np.fr>
2022-05-19 09:28:51 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setup(name="pyrough",
+setup(name="Pyrough",
 version="1.0",
 description="A tool for rough samples constructions",
 author="Hugo Iteney",
+url = "https://github.com/HugoIte/Pyrough",
 author_email="hugo.iteney@im2np.fr",
 packages=find_packages(),
 install_requires=requirements
-]
       )
-
-#ATOMSK_PATH=''/usr/local/bin/atomsk''
-#GMSH_PATH=/home/hiteney/.local/bin/gmsh
