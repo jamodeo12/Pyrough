c1021235615cef3900045f3f2438e5316fb3dcfd
hiteney <hugo.iteney@im2np.fr>
2023-05-05 11:15:26 +0200

    new doc style

--- docs/files/conf.py
+++ docs/files/conf.py
@@ -91,6 +91,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {'logo_only': True, 'style_nav_header_background': 'white'}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
